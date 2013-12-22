d71168b0f4d085e69d7360298cc94b715897221c
Yigal Korman <ykorman@gmail.com>
2013-12-22 20:17:51 +0000

    next chapter in progress

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,4 +4,5 @@ from rango import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
 )
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    return HttpResponse("Rango says hello world!<br><a href=\"about/\">About page</a>")
+
+def about(request):
+    return HttpResponse("Rango says: Here is the about page.<br><a href=\"..\">Main page</a>")
 
