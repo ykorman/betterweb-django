c060d30cb98632cb4dfb7d465ab8bac082f9d760
Yigal Korman <ykorman@gmail.com>
2014-01-03 12:55:03 +0000

    configured userena

--- betterweb_project/betterweb_project/urls.py
+++ betterweb_project/betterweb_project/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^bw/', include('betterweb_app.urls')),
+    url(r'^accounts/', include('userena.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
